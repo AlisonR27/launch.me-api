3282019e35b8d46e09d73532e85bdb442276ab4d
Alison Ranier <alisonranier@gmail.com>
2021-06-11 15:31:02 -0300

    Coding the run over timeout of the authentication. Modification of the functions in AuthController.

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,26 @@
 'use strict';
+
+const { response } = require('express');
+
   const getToken  = () => {
+    setTimeout(() => { 
+      auth().then((response)=>{
+        console.log("Próxima execução do autenticador: "+ (response.expires_in)/60 +"min");
+      })
+    }, response.expires_in*1000)
+  }
+  const auth = () => {
     const axios = require('axios');
     const igdbLink = `https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_KEY}&grant_type=client_credentials`;
-    axios.post(`${igdbLink}`)
-    .then(function(response) {
-      process.env.IGDB_TOKEN = response.data.access_token
-      setTimeout(() => { 
-        getToken()
-      }, response.expires_in*1000)
-    }).catch(err => console.log(err))
-    }
+    return new Promise((resolve,reject) => {
+      axios.post(`${igdbLink}`)
+      .then(function(response) {
+        process.env.IGDB_TOKEN = response.data.access_token
+        resolve(response.data)
+      }).catch(err => reject(err))
+    })
+    
+  }  
 module.exports = {
   getToken
 }
